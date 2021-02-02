25870ac34f3bbb005ea0b345b58d38efbcba54b6
Sasa Milosevic <sasmilo20@gmail.com>
2021-02-02 19:11:20 +0100

    Website filled with content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,336 @@
-import logo from './logo.svg';
-import './App.css';
-
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import dashboard from './images/dealwithgrowth-dashboard-header.jpg';
+import logo2 from './images/dealwithgrowth.com.dark.png';
+import logo from './images/dealwithgrowth.com.light.png';
+import jeremy from './images/jeremy.jpg';
+import klaviyo from './images/klaviyo_white_logo.png';
+import shopify from './images/shopify-partner-crop.png';
+import spain from './images/spain-flag.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        css={css`
+          background: #fff;
+        `}
+      >
+        <header>
+          <ul>
+            <li>
+              <img src={logo} alt="dealwithgrowth" />
+            </li>
+            <li>
+              <a href="#1">Work</a>
+            </li>
+            <li>
+              <a href="#1">Why Klaviyo?</a>
+            </li>
+            <li>
+              <a href="#1">Blog</a>
+            </li>
+            <li>
+              <a href="#1">About</a>
+            </li>
+            <li>
+              <a href="#1">Get in touch</a>
+            </li>
+            <li>
+              <a href="#1">Language</a>
+            </li>
+          </ul>
+        </header>
+
+        <section>
+          <h1>Email campaigns that run your entire Shopify store</h1>
+          <p>
+            ne & execute sophisticated email strategies that delight ou can
+            drive a steady stream of revenue and repeat purchases to your store
+            without relying on aggressive discounting.
+          </p>
+          <button>Find out how</button>
+          <a href="#1">See examples of work</a>
+          <img src={dashboard} alt="Merchants on its Shopify dashboard" />
+        </section>
+
+        <section>
+          <h2>Your email list can net you an extra 10-20% more revenue</h2>
+          <p>
+            face it, your current email strategy is stuck in the stone sting
+            customers—leaving a chases on the table.You know, the kind of
+            profitable purchases you won’t get with Facebook Ads.
+          </p>
+          <p>
+            le, brands like Harry’s, Chubbies and Thirdlove are nailing me while
+            building long-term customer relationships.But y any of what they’re
+            doing to your our plate trying to hit yet another sales target next
+            month.
+          </p>
+          <p>pen?How do you ensure you only send rs buying again & again?</p>
+          <p>Well, you don’t have to—because I’m here to do it all for you.</p>
+        </section>
+
+        <section>
+          <h2>Make email the best asset for your Shopify store</h2>
+          <p>
+            Hi, I’m <a href="#1">Jeremy</a>, ecommerce consultant and former
+            marketing specialist at SPOKE, one of the fastest growing consumer
+            businesses in the UK.
+          </p>
+          <p>
+            eauty brands (just like yours!) on Shopify to il campaigns with
+            Klaviyo, letting you:
+          </p>
+          <ul>
+            <li>Turn subscribers into paying customers</li>
+            <li>Recover lost sales</li>
+            <li>Retain customers and maximize their LTV</li>
+          </ul>
+
+          <p>
+            Ultimately, being there whenever your customers need you—so you can
+            build a sustainable business with predictable, repeatable sales.
+          </p>
+          <img src={jeremy} alt="jeremy" />
+        </section>
+        <section>
+          <h2>Solutions to step up your email marketing game</h2>
+        </section>
+        <section>
+          <div>
+            <h3>Email automation audit & strategy</h3>
+            <p>
+              If you feel you’ve neglected email for a long time or if you’re
+              looking to give your email strategy a facelift, then this is the
+              service for you. After running a rigorous audit and data analysis,
+              you’ll get a comprehensive report packed with proven strategies
+              that will increase engagement and start generating sales right
+              away.
+            </p>
+            <button>Get in touch</button>
+          </div>
+          <div>
+            <h3>Done for you Klaviyo email automation</h3>
+            <p>
+              Swamped with projects? Looking for a partner with hands-on
+              experience to make the most out of Klaviyo? Look no further. With
+              my done-for-you services, you’ll get finely tuned email campaigns
+              designed to delight your customers and send your LTV through the
+              ceiling.
+            </p>
+            <button>Are we a match?</button>
+          </div>
+        </section>
+        <section>
+          <h2>
+            These DTC brands on Shopify are now thriving with email marketing
+          </h2>
+          <ul>
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>{' '}
+            <li>
+              <a
+                href="#1"
+                target="_blank"
+                rel="noopener noreferrer"
+                alt="Black Limba"
+              >
+                {' '}
+              </a>
+            </li>
+          </ul>
+          <a href="#1" alt="link">
+            Stg
+          </a>
+        </section>
+        <section>
+          <h2>A proven track record to drive meaningful ROI through emails</h2>
+          <p>
+            Delighting your customers is good business. But as an entrepreneur,
+            you care about the hard, cold numbers. The kind of numbers you can
+            take to the bank.
+          </p>
+          <p>
+            So here’s a typical example of what you can expect: one of my
+            clients in the men’s lifestyle industry generates almost 20% of his
+            monthly revenue from the automated email campaigns we’ve created:
+          </p>
+          <img src={dashboard} alt="dashboard" />
+          <p>
+            By first auditing the current email strategy and learning more about
+            shoppers using visitors polls, we were able to build an automated
+            personalized customer experience for all stages of the funnel.
+          </p>
+          <p>
+            Combined with segmented newsletters, his email channel now accounts
+            for more than 30% of his monthly sales, while creating lasting
+            relationships with customers, respecting their inboxes and making
+            sure they’re a valued part of the community.
+          </p>
+          <p>Want me to do the same for you?</p>
+          <button> </button>
+        </section>
+        <section>
+          <h2>Get the most out of your email marketing</h2>
+          <p>
+            30 days from now, you could already be making more sales from your
+            automated email campaigns. Stop neglecting them—get in touch today
+            to find out how you can drive meaningful ROI and send your customer
+            LTV through the ceiling.
+          </p>
+          <button>Let's talk email </button>
+          <p>
+            Not ready to get in touch yet?{' '}
+            <a href="#1" alt="link33">
+              {' '}
+              Get the strategies to recover 15% of your lost Shopify sales →
+            </a>
+          </p>
+        </section>
+        <section>
+          <div>
+            <img src={logo2} alt="dealwithgrowth" />
+            <p>
+              Helping Shopify merchants <br />
+              to thrive
+            </p>
+          </div>
+          <div>
+            <h3>Information</h3>
+            <ul>
+              <li>Contact</li>
+              <li>About</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Partners</h3>
+            <a href="#1" alt="shopify">
+              <img src={shopify} alt="shopify" />
+            </a>
+            <a href="#1" alt="klaviyo">
+              <img src={klaviyo} alt="klaviyo" />
+            </a>
+          </div>
+          <div>
+            <h3>International</h3>
+            <ul>
+              <li>
+                <a href="#1" alt="">
+                  English Version
+                </a>
+              </li>
+              <li>
+                <a href="#1" alt="">
+                  Version Française
+                </a>
+              </li>
+              <li>
+                <a href="#1" alt="">
+                  Versión Española
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <footer>
+          <p>
+            © 2021 · <a href="/">Dealwithgrowth</a> · Ecommerce consultancy
+            powered by{' '}
+            <a href="https://whois.jeremyrieunier.com/">Jeremy Rieunier</a> ·
+            Currently based in sunny Valencia
+            <img src={spain} alt="spain" />
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
